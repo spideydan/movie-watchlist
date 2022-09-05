3ed7dd9c370bb5d3e296d0b0399ac5aea81d25a5
Mark Halverstadt <markhalverstadt@gmail.com>
2022-09-05 15:17:06 -0600

    trying to link TMDB API

--- routes/movies.js
+++ routes/movies.js
@@ -5,7 +5,7 @@ const { ensureAuth } = require('../middleware/auth')
 
 router.get('/', ensureAuth, moviesController.getMovies)
 
-router.post('/addMovie', moviesController.addMovie)
+router.get('/addMovie', moviesController.addMovie)
 
 router.put('/watched', moviesController.watched)
 
@@ -14,4 +14,4 @@ router.put('/unwatched', moviesController.unwatched)
 router.delete('/deleteMovie', moviesController.deleteMovie)
 
 module.exports = router
-
+ 
