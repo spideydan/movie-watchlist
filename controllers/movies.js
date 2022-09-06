8a2c57a5545ba0b97cf8ac466f617250a0f8c1ee
your_github_username <kyle.huang.dev@gmail.com>
2022-09-06 09:54:25 -0600

    Edit the gitignore

--- controllers/movies.js
+++ controllers/movies.js
@@ -9,7 +9,7 @@ module.exports = {
         try {
             const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`);
             const data = await response.json();
-            console.log(data); 
+            // console.log(data); 
             const movieItems = await Movie.find({ userId: req.user.id })
             const itemsLeft = await Movie.countDocuments({ userId: req.user.id, completed: false })
             res.render('movies.ejs', { movies: movieItems, left: itemsLeft, user: req.user, data:data})
